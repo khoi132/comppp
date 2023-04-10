de27f0884e0e99f6e4f28defb7b4c6dcf478328e
khoi132 <117442860+khoi132@users.noreply.github.com>
2023-04-10 18:55:25 +0700

    docker

--- src/connect.php
+++ src/connect.php
@@ -1,7 +1,7 @@
 <?php
-$server = "localhost";
-$user="root";
-$pass="";
+$server = "db";
+$user="khoitv";
+$pass="khoitv@123";
 $database="cc_computing";
 $conn=mysqli_connect($server,$user,$pass,$database);
 mysqli_query($conn,'set names "utf8"');
