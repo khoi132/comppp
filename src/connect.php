418b5e3e85ff87772a622874e4faa6f6aab29152
khoi132 <117442860+khoi132@users.noreply.github.com>
2023-04-12 19:32:34 +0700

    edit connect.php

--- src/connect.php
+++ src/connect.php
@@ -3,6 +3,9 @@ $server = "db";
 $user="khoitv";
 $pass="khoitv@123";
 $database="cc_computing";
-$conn=mysqli_connect($server,$user,$pass,$database);
+$mysqli = new mysqli($server,$user,$pass,$database);
 mysqli_query($conn,'set names "utf8"');
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
 ?>
